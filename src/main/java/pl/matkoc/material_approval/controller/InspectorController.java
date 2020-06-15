d5b21aeda062eef4e73c0f93585093c638483fff
Mateusz Kocierz <mateusz_kocierz@wp.pl>
2020-06-15 20:51:46 +0200

    Add action show material list and edit each material 15_06_2020

--- src/main/java/pl/matkoc/material_approval/controller/InspectorController.java
+++ src/main/java/pl/matkoc/material_approval/controller/InspectorController.java
@@ -1,9 +1,13 @@
 package pl.matkoc.material_approval.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import pl.matkoc.material_approval.domain.dao.InspectorDao;
+import pl.matkoc.material_approval.domain.dao.MaterialDao;
+import pl.matkoc.material_approval.domain.model.Material;
 
 
 @Controller
@@ -11,13 +15,30 @@ import pl.matkoc.material_approval.domain.dao.InspectorDao;
 public class InspectorController {
 
     private final InspectorDao inspectorDao;
+    private final MaterialDao materialDao;
 
-    public InspectorController(InspectorDao inspectorDao) {
+    public InspectorController(InspectorDao inspectorDao, MaterialDao materialDao) {
         this.inspectorDao = inspectorDao;
+        this.materialDao = materialDao;
     }
 
     @GetMapping("/inspector/action")
     public String actionInsp(){
         return "inspector/action";
+    }
+
+    // wyświetlanie listy materiałów
+    @GetMapping("/listMaterial")
+    public String showListMaterialToConfirm(Model model){
+        model.addAttribute("confirmList", materialDao.findAll());
+        return "inspector/editList";
+    }
+
+    // zatwierdzenie materiału
+    @GetMapping("/inspector/confirmMaterial")
+    public String prepareConfirmedMaterialList(Model model, @RequestParam long id){
+        Material material = materialDao.readById(id);
+        model.addAttribute("confirmList", material);
+        return "inspector/confirmMaterial";
     }
 }
